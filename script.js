4dac455ff4490db2c02ce398094da8a265bf1e9d
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-07-28 14:34:01 -0700

    pseudo code examples

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 // storing all characters in variables.
-
+var generateBtn = document.querySelector("#generate");
 // numbers
+var numbers = '0123456789'.split("");
 // var numbers = [1,2,3];
 
 // special characters
@@ -12,10 +13,22 @@
 // uppercase letters
 
 
-var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-
+// ask for number of characters
 }
+// check if it is valid
+
+// confirm it
+
+// use if true
+
+// lower case letters
+
+// use uppercase letters
+
+// use numbers
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
