e0af5f8bbbe17924d2ce04d2017c3221f90b1ea8
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-07-28 02:06:36 -0700

    first draft

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+//storing all characters in variables.
+
+// numbers
+// var numbers = [1,2,3];
+
+// special characters
+// var specialCharacters =
+
+
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
